4199f3538b63e7e4dcc7eff46008046887d296ed
Ryan Fitzpatrick <email@theryanfitzpatrick.com>
2017-11-27 19:25:29 -0500

    aded new file

--- Source/Tree.c
+++ Source/Tree.c
@@ -75,16 +75,16 @@ static lsTree ** Expand(lsTree * parent, unsigned char node)
     switch(node)
     {
         case 0:
-            lsBuildSystem(children[0], parent, 1, 2);
-            lsBuildSystem(children[1], parent, 3, 1);
+            lsBuildSystem(children[0], parent, 1, -2);
+            lsBuildSystem(children[1], parent, 3, -1);
             lsBuildSystem(children[2], parent, 1, 0);
             lsBuildSystem(children[3], parent, 3, -1);
             return children;
         case 1:
-            lsBuildSystem(children[0], parent, 1, 4);
-            lsBuildSystem(children[1], parent, 2, 3);
-            lsBuildSystem(children[2], parent, 0, 4);
-            lsBuildSystem(children[3], parent, 1, 2);
+            lsBuildSystem(children[0], parent, 1, 1);
+            lsBuildSystem(children[1], parent, 2, -3);
+            lsBuildSystem(children[2], parent, 0, 2);
+            lsBuildSystem(children[3], parent, 1, -3);
             return children;
         case 2:
             lsBuildSystem(children[0], parent, 3, -2);
@@ -96,10 +96,10 @@ static lsTree ** Expand(lsTree * parent, unsigned char node)
             lsBuildSystem(children[0], parent, 0, -2);
             lsBuildSystem(children[1], parent, 0, 1);
             lsBuildSystem(children[2], parent, 2, 0);
-            lsBuildSystem(children[3], parent, 2, 0);
+            lsBuildSystem(children[3], parent, 2, -1);
             return children;
         default:
-            lsBuildSystem(children[0], parent, 3, 3);
+            lsBuildSystem(children[0], parent, 3, -3);
             lsBuildSystem(children[1], parent, 2, 3);
             lsBuildSystem(children[2], parent, 3, -1);
             lsBuildSystem(children[3], parent, 3, 2);
